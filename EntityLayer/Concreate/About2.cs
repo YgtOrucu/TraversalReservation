ebc4afbbbafb62060f937873df9b21cb6026a418
YİĞİT ÖRÜCÜ <orucuyigit@gmail.com>
2025-12-24 22:04:32 +0300

    Add project files.

---
+++ EntityLayer/Concreate/About2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concreate
+{
+    public class About2
+    {
+        public int AboutID { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ EntityLayer/Concreate/NewsLatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concreate
+{
+    public class NewsLatter
+    {
+        public int NewsLatterID { get; set; }
+        public string Mail { get; set; }
+        public bool Status { get; set; }
+    }
+}
